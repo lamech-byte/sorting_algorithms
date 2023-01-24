61eae72a21471fd78602756a01136e5664a49173
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-01-24 22:06:05 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -31,4 +31,10 @@ void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void heapify(int *array, size_t s, size_t root, size_t size);
+void radix_sort(int *array, size_t size);
+void count_sort_LSD(int *array, size_t size, size_t lsd);
+void quick_sort_hoare(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
+void bitonic_merge(int *array, int l, int r, int direction);
 #endif
